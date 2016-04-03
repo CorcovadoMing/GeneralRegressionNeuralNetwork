49987301b34cf3febf5b25a40f0bbd0359a3dfab
CorcovadoMing <rf37535@gmail.com>
2016-04-03 22:15:26 +0800

    Add test data

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "grnn.h"
 #include <vector>
+#include <iostream>
 
 int main() {
-	std::vector<double> input {0}, train_y {0};
-	std::vector< std::vector<double> > train_x { {0} };
-	double grnn_output = grnn(input, train_x, train_y, 0);
+
+	std::vector<double> train_y {-10, -10, -10, -10, -10, -10, -10, -10, -10, 0, 0, 10, 10, 10};
+	std::vector< std::vector<double> > train_x { {3, 5}, {3, 11}, {8, 6}, {0, 34}, {13, 3}, {2, 17}, {23, 2}, {37, 1}, {1, 40}, {21, 30}, {30, 24}, {24, 64}, {43, 46}, {31, 51} };
+
+	std::vector< std::vector<double> > input { {25, 21} };
+	
+	for (int i = 0; i < input.size(); i += 1) {
+		std::cout << grnn(input[i], train_x, train_y, 400) << std::endl;
+	}
+
 	return 0;
 }
