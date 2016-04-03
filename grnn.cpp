e457595e21a70061714bdb3be92bc2cfcdcfcd9a
CorcovadoMing <rf37535@gmail.com>
2016-04-03 21:49:26 +0800

    Add activator function

--- grnn.cpp
+++ grnn.cpp
@@ -1,5 +1,10 @@
 #include "grnn.h"
 
+const double activator(const double input, const double xi, const double sigma) {
+	double output = 0;
+	return output;
+}
+
 const double grnn(const double input, const std::vector<double> train_x, const std::vector<double> train_y, const double sigma) {
 	double output = 0;
 
--- grnn.h
+++ grnn.h
@@ -2,4 +2,5 @@
 
 #include <vector>
 
+const double activator(const double, const double, const double);
 const double grnn(const double, const std::vector<double>, const std::vector<double>, const double);
