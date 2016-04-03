9b177d014a13b7e14f3cc66a28dc78306a1575d3
CorcovadoMing <rf37535@gmail.com>
2016-04-03 21:56:26 +0800

    Done grnn algorithm

--- grnn.cpp
+++ grnn.cpp
@@ -2,13 +2,15 @@
 #include "grnn.h"
 
 const double activator(const double input, const double xi, const double sigma) {
-	double output = 0;
 	double distance = std::pow(input-xi, 2);
 	return std::exp(-distance/sigma);
 }
 
 const double grnn(const double input, const std::vector<double> train_x, const std::vector<double> train_y, const double sigma) {
-	double output = 0;
-
-	return output;
+	double factor = 0, divide = 0;
+	for (int i = 0; i < train_x.size(); i += 1) {
+		factor += train_y[i]*activator(input, train_x[i], sigma);
+		divide += activator(input, train_x[i], sigma);
+	}
+	return factor/divide; 
 }
