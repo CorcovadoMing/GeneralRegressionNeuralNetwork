71e945dcc5aba4c4d2d940bbc64f0bcd822bf554
CorcovadoMing <rf37535@gmail.com>
2016-04-03 21:52:41 +0800

    Done activator function

--- grnn.cpp
+++ grnn.cpp
@@ -1,13 +1,14 @@
+#include <cmath>
 #include "grnn.h"
 
 const double activator(const double input, const double xi, const double sigma) {
 	double output = 0;
-	return output;
+	double distance = std::pow(input-xi, 2);
+	return std::exp(-distance/sigma);
 }
 
 const double grnn(const double input, const std::vector<double> train_x, const std::vector<double> train_y, const double sigma) {
 	double output = 0;
-
 
 	return output;
 }
