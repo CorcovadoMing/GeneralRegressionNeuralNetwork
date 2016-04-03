561273eb5c7caf77de7d8a9351d3a7dda8277c81
CorcovadoMing <rf37535@gmail.com>
2016-04-03 22:07:59 +0800

    Dimension of data could be more than one

--- grnn.cpp
+++ grnn.cpp
@@ -1,12 +1,15 @@
 #include <cmath>
 #include "grnn.h"
 
-const double activator(const double input, const double xi, const double sigma) {
-	double distance = std::pow(input-xi, 2);
+const double activator(const std::vector<double> input, const std::vector<double> xi, const double sigma) {
+	double distance = 0;
+	for(int i = 0; i < input.size(); i += 1) {
+		distance += std::pow(input[i] - xi[i], 2);
+	}
 	return std::exp(-distance/sigma);
 }
 
-const double grnn(const double input, const std::vector<double> train_x, const std::vector<double> train_y, const double sigma) {
+const double grnn(const std::vector<double> input, const std::vector< std::vector<double> > train_x, const std::vector<double> train_y, const double sigma) {
 	double factor = 0, divide = 0;
 	for (int i = 0; i < train_x.size(); i += 1) {
 		factor += train_y[i]*activator(input, train_x[i], sigma);
--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 
 int main() {
-	std::vector<double> train_x, train_y;
-	double grnn_output = grnn(0, train_x, train_y, 0);
+	std::vector<double> input {0}, train_y {0};
+	std::vector< std::vector<double> > train_x { {0} };
+	double grnn_output = grnn(input, train_x, train_y, 0);
 	return 0;
 }
