8b49b44283d7c366374c9b741bc81e21dce32826
CorcovadoMing <rf37535@gmail.com>
2016-07-04 01:28:31 +0800

    Add python implementation; fix bugs in c++

--- src/grnn.cpp
+++ src/grnn.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <iostream>
 #include "grnn.h"
 
 const double activator(const std::vector<double> input, const std::vector<double> x, const double sigma) {
@@ -6,7 +7,7 @@ const double activator(const std::vector<double> input, const std::vector<double
 	for(int i = 0; i < input.size(); i += 1) {
 		distance += std::pow(input[i] - x[i], 2);
 	}
-	return std::exp(-distance/sigma);
+	return std::exp(-distance / std::pow(sigma, 2));
 }
 
 const std::vector<double> grnn(const std::vector<double> input, const std::vector< std::vector<double> > train_x, const std::vector< std::vector<double> > train_y, const double sigma) {
@@ -14,8 +15,9 @@ const std::vector<double> grnn(const std::vector<double> input, const std::vecto
 	for (int dim = 0; dim < train_y[0].size(); dim += 1) {
 		double factor = 0, divide = 0;
 		for (int i = 0; i < train_x.size(); i += 1) {
-			factor += train_y[i][dim]*activator(input, train_x[i], sigma);
-			divide += activator(input, train_x[i], sigma);
+            double cache = activator(input, train_x[i], sigma);
+			factor += train_y[i][dim] * cache;
+			divide += cache;
 		}
 		result.push_back(factor/divide);
 	}
